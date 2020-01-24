d6efe165ece1758f035f9235126f482cbbf27ce9
Richard Borge <richard.borge@nav.no>
2020-01-24 11:56:59 +0100

    Legger til land på arbeidsforhold

--- src/main/kotlin/no/nav/medlemskap/domene/Arbeidsforhold.kt
+++ src/main/kotlin/no/nav/medlemskap/domene/Arbeidsforhold.kt
@@ -2,8 +2,16 @@ package no.nav.medlemskap.domene
 
 import java.time.YearMonth
 
-data class Arbeidsforhold(
-        val utenlandsopphold: List<Utenlandsopphold>
+data class Arbeidsforhold (
+        val periode: Periode,
+        val utenlandsopphold: List<Utenlandsopphold>,
+        val arbeidsgiver: Arbeidsgiver
+)
+
+data class Arbeidsgiver (
+        val type: String,
+        val identifikator: String,
+        val landkode: String // Må kanskje hentes fra https://data.brreg.no/enhetsregisteret/api/docs/index.html#enheter-oppslag
 )
 
 data class Utenlandsopphold (
