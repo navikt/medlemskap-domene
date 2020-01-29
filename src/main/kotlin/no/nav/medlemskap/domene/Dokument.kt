1cfd34cb0f111b72e540f43dc27074895a9a1d57
Silje Risvik <silje.risvik@nav.no>
2020-01-29 16:27:02 +0100

    En del kan vre null i Journalpost..

--- src/main/kotlin/no/nav/medlemskap/domene/Dokument.kt
+++ src/main/kotlin/no/nav/medlemskap/domene/Dokument.kt
@@ -2,10 +2,10 @@ package no.nav.medlemskap.domene
 
 data class Journalpost (
         val journalpostId: String,
-        val tittel: String,
-        val journalposttype: String,
-        val journalstatus: String,
-        val tema: String,
+        val tittel: String?,
+        val journalposttype: String?,
+        val journalstatus: String?,
+        val tema: String?,
         val dokumenter: List<Dokument>?
 )
 
